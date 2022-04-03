64dc14150301ff306ce934022c815b3d8fa32b6b
ceyhanbekiroglu <ceyhanbekiroglu@gmail.com>
2022-04-03 19:45:06 +0200

    Add `Balance`, `IncomeExpenses`, `TransactionList` and `AddTransaction`

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from "react"
 import "./App.css"
+import { Header } from "./Components/Header"
+import Balance from "./Components/Balance"
+import IncomeExpenses from "./Components/IncomeExpenses"
+import TransactionList from "./Components/TransactionList"
+import AddTransaction from "./Components/AddTransaction"
+
+import { GlobalProvider } from "./context/GlobalState"
 
 function App() {
-	return <div></div>
+	return (
+		<GlobalProvider>
+			<Header />
+			<div className="container">
+				<Balance />
+				<IncomeExpenses />
+				<TransactionList />
+				<AddTransaction />
+			</div>
+		</GlobalProvider>
+	)
 }
 
 export default App
