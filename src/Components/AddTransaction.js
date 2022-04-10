b9da4b5644c75195d3119c615517a6aa368beb40
ceyhanbekiroglu <ceyhanbekiroglu@gmail.com>
2022-04-10 09:15:19 +0200

    Add `submit` ftunction

--- src/Components/AddTransaction.js
+++ src/Components/AddTransaction.js
@@ -1,14 +1,30 @@
 import { useState } from "react"
 import React from "react"
+import { useContext } from "react"
+import { GlobalContext } from "../context/GlobalState"
 
 export default function AddTransaction() {
 	const [text, setText] = useState("")
 	const [amount, setAmount] = useState(0)
 
+	const { addTransaction } = useContext(GlobalContext)
+
+	const onSubmit = (e) => {
+		e.preventDefault()
+
+		const newTransaction = {
+			id: Math.floor(Math.random() * 100000000),
+			text,
+			amount: +amount,
+		}
+
+		addTransaction(newTransaction)
+	}
+
 	return (
 		<div>
 			<h3>Add new transaction</h3>
-			<form>
+			<form onSubmit={onSubmit}>
 				<div className="form-control">
 					<label htmlFor="text">Text</label>
 					<input
